ffaf6a3b0860406bdcfc9b0493d109203cc8c8a3
songzuling <1024511362@qq.com>
2018-07-10 22:13:34 +0800

    1.z轴代码逻辑基本完成，用行程开关测试了一下逻辑没问题，还没在实体测试。2.还没将各种故障情况考虑完全，需要加入

--- Core/Src/zaxis.c
+++ Core/Src/zaxis.c
@@ -8,7 +8,7 @@ void ZAxis_Init(void)
 {
   
   /*初始化各层开关检测*/
-  for(int i=0; i<LAYER_OF_CELL; i++)
+  for(int i=0; i<SWITCH_COUNT; i++)
   {
     ZAxis.LayerSwitch[i].Index = i;
     ZAxis.LayerSwitch[i].OnDoubleClickEnable = 0;
@@ -25,37 +25,62 @@ void ZAxis_Init(void)
   
 }
 
+// 扫描开关
 void SwitchScan(void)
 {
-  KeyScan(&(ZAxis.LayerSwitch[0]),1,!HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_0));
-  KeyScan(&(ZAxis.LayerSwitch[1]),1,!HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_9));
-  KeyScan(&(ZAxis.LayerSwitch[2]),1,!HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_10));  
+  KeyScan(&(ZAxis.LayerSwitch[1]),1,!HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_0));
+  KeyScan(&(ZAxis.LayerSwitch[2]),1,!HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_9));
+  KeyScan(&(ZAxis.LayerSwitch[3]),1,!HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_10));  
   return;
 }
 
-
+uint8_t getlayer = 0;
 void ZAxisMain(void)
 {
   uint8_t shouldexit = 0;
   while(!shouldexit)
   {
     /*收到出货消息，需要去接货*/
-    if(ZAxis.IsDeliver == CELL_DELIVER)
+    if(ZAxis.IsDeliver == CMD_MASTER_DELIVER)
     {
-      for(uint8_t i=0; i<LAYER_OF_CELL; i++)
+      ZAxis.IsDeliver = CMD_NONE;
+      for(uint8_t i=0; i<SWITCH_COUNT; i++)
       {
         /*该层需要出货*/
         if(ZAxis.DeliverCellCount[i] > 0)
         {
           GoToLayer(ZAxis.CurrentLayer, i);
+          getlayer = i;
+//          if(0 == WaitingForGoods(i, 20000))
+//          {
+//            /*出货超时*/
+//            break;
+//          }
         }
         else
         {
           continue;
         }
       }// enf for
+      
+      /*接货完成，出口所在层出货*/ 
+      GoToLayer(ZAxis.CurrentLayer, LAYER_OF_OUTPUT);
     }// end if
   }
+}
+
+// 等待某层的货物出货完毕
+uint8_t WaitingForGoods(int layer, uint32_t timeout)
+{
+  uint32_t tickstart = HAL_GetTick();
+  while(ZAxis.DeliverCellCount[layer] > 0)
+  {
+    if(HAL_GetTick() - tickstart > timeout)
+    {
+      return 0;
+    }
+  }
+  return 1;
 }
 
 // 运动到指定的层
@@ -64,11 +89,11 @@ void GoToLayer(int curLayer, int layer)
   /*设定运动方向*/  
   if(curLayer - layer > 0)
   {
-    SetMovingDirection(ZAXIS_MOVE_DOWN);
+    SetMoveDirection(ZAXIS_MOVE_DOWN);
   }
   else if(curLayer - layer < 0)
   {
-    SetMovingDirection(ZAXIS_MOVE_UP);
+    SetMoveDirection(ZAXIS_MOVE_UP);
   }
   /*要去层等于当前层，可能正在当前层，也可能在
     当前层上面或者下面未到达其他层的地方*/
@@ -77,39 +102,38 @@ void GoToLayer(int curLayer, int layer)
     /*之前向上运动，现在向下*/
     if(ZAxis.MoveDirection == ZAXIS_MOVE_UP)
     {
-      SetMovingDirection(ZAXIS_MOVE_DOWN);
+      SetMoveDirection(ZAXIS_MOVE_DOWN);
     }
     /*之前向下运动，现在向上*/
     else if(ZAxis.MoveDirection == ZAXIS_MOVE_DOWN)
     {
-      SetMovingDirection(ZAXIS_MOVE_UP);
+      SetMoveDirection(ZAXIS_MOVE_UP);
     }
     else
     {
       
-    } 
-
-    /**/
-    while(ZAxis.LayerSwitch[layer].IsDown == 0)
-    { 
-      ///////////////////////////////////////////////
-      //注意添加运动过程中触底的情况判断
-      ///////////////////////////////////////////////
-      
-      SetDuty(&htim4,TIM_CHANNEL_1,50);
-      SetDuty(&htim4,TIM_CHANNEL_2,50);
-      SetDuty(&htim4,TIM_CHANNEL_3,50);
-      SetDuty(&htim4,TIM_CHANNEL_4,50);         
     }
-    SetDuty(&htim4,TIM_CHANNEL_1,0);
-    SetDuty(&htim4,TIM_CHANNEL_2,0);
-    SetDuty(&htim4,TIM_CHANNEL_3,0);
-    SetDuty(&htim4,TIM_CHANNEL_4,0);    
   }// end if
+      /**/
+  while(ZAxis.LayerSwitch[layer].IsDown == 0)
+  { 
+    ///////////////////////////////////////////////
+    //注意添加运动过程中触底的情况判断
+    ///////////////////////////////////////////////
+    
+    SetDuty(&htim4,TIM_CHANNEL_1,50);
+    SetDuty(&htim4,TIM_CHANNEL_2,50);
+    SetDuty(&htim4,TIM_CHANNEL_3,50);
+    SetDuty(&htim4,TIM_CHANNEL_4,50);         
+  }
+  SetDuty(&htim4,TIM_CHANNEL_1,0);
+  SetDuty(&htim4,TIM_CHANNEL_2,0);
+  SetDuty(&htim4,TIM_CHANNEL_3,0);
+  SetDuty(&htim4,TIM_CHANNEL_4,0);    
   
 }
     
-void SetMovingDirection(uint8_t dir)
+void SetMoveDirection(uint8_t dir)
 {
   if(dir == ZAXIS_MOVE_UP)
   {
@@ -149,14 +173,32 @@ void PraseCommand(CAN_HandleTypeDef* hcan)
     switch(hcan->pRxMsg->Data[0])
     {
       /*收到出货通知*/
-      case CELL_DELIVER:
-        ZAxis.IsDeliver = CELL_DELIVER;   
+      case CMD_MASTER_DELIVER:
+        ZAxis.IsDeliver = CMD_MASTER_DELIVER;   
         row = hcan->pRxMsg->Data[5];
-        if(row <= LAYER_OF_CELL)
+        if(row <= SWITCH_COUNT)
         {
+          if(row == LAYER_OF_OUTPUT)
+          {
+            row++;
+          }
           ZAxis.DeliverCellCount[row]++;
         }
         break;
+        
+      /*单元体出货完成*/
+      case CMD_CELL_DELIVER_DONE:        
+        row = hcan->pRxMsg->Data[5];
+        if(row <= SWITCH_COUNT)
+        {
+          if(row == LAYER_OF_OUTPUT)
+          {
+            row++;
+          }
+          ZAxis.DeliverCellCount[row]--;
+        }
+        break;
+                
       default:;
       break;
     }    
