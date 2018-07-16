0dd729d575eaaf123561c52ccbafbc8122e7536d
songzuling <1024511362@qq.com>
2018-07-16 09:29:18 +0800

    1:电机驱动的启动和停止采用改变脉冲的方式，这样在停转时候电机由保持力矩，而使用应交使能信号没有保持力矩。 2:与单元体的交互测试ok

--- Core/Src/zaxis.c
+++ Core/Src/zaxis.c
@@ -3,6 +3,7 @@
 
 ZAxis_TypeDef ZAxis;
 extern TIM_HandleTypeDef htim4;
+extern CAN_HandleTypeDef hcan;
 
 void ZAxis_Init(void)
 {
@@ -11,21 +12,26 @@ void ZAxis_Init(void)
   for(int i=0; i<SWITCH_COUNT; i++)
   {
     ZAxis.LayerSwitch[i].Index = i;
-    ZAxis.LayerSwitch[i].OnDoubleClickEnable = 0;
-    ZAxis.LayerSwitch[i].OnKeyDownEnable = 1;
-    ZAxis.LayerSwitch[i].OnKeyPressEnable = 0;
-    ZAxis.LayerSwitch[i].OnKeyUpEnable = 1;
     ZAxis.LayerSwitch[i].EventOnDoubleClick = DoNothing;
     ZAxis.LayerSwitch[i].EventOnKeyDown = OnLayerSwitchDown;
     ZAxis.LayerSwitch[i].EventOnKeyPress = DoNothing;
     ZAxis.LayerSwitch[i].EventOnKeyUp = DoNothing;
   }
   
-  SetDuty(&htim4,TIM_CHANNEL_1,50);
-  SetDuty(&htim4,TIM_CHANNEL_2,50);
-  SetDuty(&htim4,TIM_CHANNEL_3,50);
-  SetDuty(&htim4,TIM_CHANNEL_4,50);  
-  
+  for(int i=0; i<2; i++)
+  {
+    ZAxis.Key[i].Index = i;
+    ZAxis.Key[i].EventOnDoubleClick = DoNothing;
+    ZAxis.Key[i].EventOnKeyDown = DoNothing;
+    ZAxis.Key[i].EventOnKeyPress = DoNothing;
+    ZAxis.Key[i].EventOnKeyUp = DoNothing;
+  }
+
+  ZAxis.CurrentLayer = 0;
+  STOP_ZAXIS;  
+  SET_ZAXIS_MOVE_UP;
+  ENABLE_ZAXIS_DRIVER;
+  SetMoveDirection(ZAXIS_MOVE_UP);
 }
 
 // 扫描开关
@@ -36,47 +42,59 @@ void SwitchScan(void)
 //  KeyScan(&(ZAxis.LayerSwitch[3]),1,!HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_10));  
   
   KeyScan(&(ZAxis.LayerSwitch[1]),1,!HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_0));
-  KeyScan(&(ZAxis.LayerSwitch[2]),1,!HAL_GPIO_ReadPin(GPIOB, GPIO_PIN_0));
-  KeyScan(&(ZAxis.LayerSwitch[3]),1,!HAL_GPIO_ReadPin(GPIOB, GPIO_PIN_1));  
+  KeyScan(&(ZAxis.Key[0]),1,!HAL_GPIO_ReadPin(GPIOB, GPIO_PIN_0));
+  KeyScan(&(ZAxis.Key[1]),1,!HAL_GPIO_ReadPin(GPIOB, GPIO_PIN_1));  
   return;
 }
 
-uint8_t getlayer = 0;
+/*
+ * @brief  Z轴主逻辑
+ * @param  空
+ * @retval 空
+ */
 void ZAxisMain(void)
 {
   uint8_t shouldexit = 0;
-  while(!shouldexit)
+  while(0==shouldexit)
   {
     /*收到出货消息，需要去接货*/
     if(ZAxis.IsDeliver == CMD_MASTER_DELIVER)
     {
       ZAxis.IsDeliver = CMD_NONE;
+      HAL_GPIO_WritePin(GPIOA, GPIO_PIN_7, GPIO_PIN_SET); 
       for(uint8_t i=0; i<SWITCH_COUNT; i++)
       {
         /*该层需要出货*/
         if(ZAxis.DeliverCellCount[i] > 0)
         {
           GoToLayer(ZAxis.CurrentLayer, i);
-          getlayer = i;
-//          if(0 == WaitingForGoods(i, 20000))
-//          {
-//            /*出货超时*/
-//            break;
-//          }
+          hcan.pTxMsg->Data[0] = CMD_ZAXIS_REACH;
+          HAL_CAN_Transmit_IT(&hcan);  
+          if(0 == WaitingForGoods(i, 100000))
+          {
+            /*出货超时*/
+            break;
+          }
         }
         else
         {
           continue;
         }
       }// enf for
-      
+      HAL_GPIO_WritePin(GPIOA, GPIO_PIN_7, GPIO_PIN_RESET); 
       /*接货完成，出口所在层出货*/ 
       GoToLayer(ZAxis.CurrentLayer, LAYER_OF_OUTPUT);
     }// end if
   }
 }
 
-// 等待某层的货物出货完毕
+
+/*
+ * @brief  等待某层的货物出货完毕
+ * @param  layer:要出货的那一层
+ * @param  timeout:等待时间
+ * @retval 0:超时  1:成功
+ */
 uint8_t WaitingForGoods(int layer, uint32_t timeout)
 {
   uint32_t tickstart = HAL_GetTick();
@@ -91,14 +109,14 @@ uint8_t WaitingForGoods(int layer, uint32_t timeout)
 }
 
 // 运动到指定的层
-void GoToLayer(int curLayer, int layer)
+void GoToLayer(int curLayer, int toLayer)
 {
   /*设定运动方向*/  
-  if(curLayer - layer > 0)
+  if(curLayer - toLayer > 0)
   {
     SetMoveDirection(ZAXIS_MOVE_DOWN);
   }
-  else if(curLayer - layer < 0)
+  else if(curLayer - toLayer < 0)
   {
     SetMoveDirection(ZAXIS_MOVE_UP);
   }
@@ -106,6 +124,7 @@ void GoToLayer(int curLayer, int layer)
   当前层上面或者下面未到达其他层的地方*/
   else
   {
+    HAL_GPIO_WritePin(GPIOA, GPIO_PIN_7, GPIO_PIN_SET); 
     /*之前向上运动，现在向下*/
     if(ZAxis.MoveDirection == ZAXIS_MOVE_UP)
     {
@@ -124,8 +143,8 @@ void GoToLayer(int curLayer, int layer)
   
   /*阻塞等待z轴到达相应层*/
   START_ZAXIS;
-  while(ZAxis.LayerSwitch[layer].IsDown == 0)
-  { 
+  while(ZAxis.LayerSwitch[toLayer].IsDown == 0)
+  {
     ///////////////////////////////////////////////
     //注意添加运动过程中触底的情况判断
     ///////////////////////////////////////////////
@@ -138,20 +157,20 @@ void SetMoveDirection(uint8_t dir)
 {
   if(dir == ZAXIS_MOVE_UP)
   {
-    /*控制引脚改变运动方向*/
-       
+    SET_ZAXIS_MOVE_UP; 
+    /*改变运动方向标志*/
+    ZAxis.MoveDirection = dir;
   }
   else if(dir == ZAXIS_MOVE_DOWN)
   {
-  
+    SET_ZAXIS_MOVE_DOWN;
+    /*改变运动方向标志*/
+    ZAxis.MoveDirection = dir;
   }
   else
   {
-  
+    /*误传入其他值，忽略*/
   }    
-  /*改变运动方向标志*/
-  ZAxis.MoveDirection = dir;
-  
   return;
 }
 
